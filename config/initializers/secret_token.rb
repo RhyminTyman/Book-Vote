e60586909e7b0badc1839d76d98afefed0f56914
Joe Tyman <josephtyman@me.com>
2014-05-25 22:32:04 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookvote::Application.config.secret_key_base = '75b68c5e1eaa9807728aee4bc1e79b42715fcd13b5d8ce6a06bf2df378502a976344e0f79d3d0210a1826a3c38ca854504fbcf279d18e32ff5ed56a0998a0d60'
